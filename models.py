e66f0866dce11705b39097506727cdb84064d058
Abdulaziz <sukhrobjonov02@gmail.com>
2023-07-10 13:27:46 +0500

    setup almost complete

--- models.py
+++ models.py
@@ -1,20 +1,32 @@
-from settings import db
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from config import DATABASE_URL
 
-shows_table = Table = db.Table('shows',
-    db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), nullable=False),
-    db.Column('actor_id', db.Integer, db.ForeignKey('actors.id'), nullable=False),
-    db.Column('start_time', db.DateTime, default=datetime.today(), nullable=False),
-    db.PrimaryKeyConstraint('movie_id', 'actor_id', 'start_time')
-)
+database_path = DATABASE_URL
+if database_path.startswith("postgres://"):
+  database_path = database_path.replace("postgres://", "postgresql://", 1)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
 
 class Movie(db.Model):
     __tablename__ = 'movies'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    release_date = db.Column(db.DateTime, nullable=False)
-    actors = db.relationship('Actor', secondary=shows_table, backref=db.backref('movies', lazy=True))
+    release_date = db.Column(db.DateTime, default=datetime.today(), nullable=False)
+    genres = db.Column(db.ARRAY(db.String()), nullable=False)
 
 class Actor(db.Model):
     __tablename__ = 'actors'
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export DATABASE_URL="postgresql://abdulaziz:whocares@127.0.0.1:5432/capstone"
+echo "setup.sh script executed successfully!"

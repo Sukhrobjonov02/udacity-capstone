9220b63253166b1ff8780d03fd972ae043ea96b3
Abdulaziz <sukhrobjonov02@gmail.com>
2023-07-10 15:11:20 +0500

    fixing database-url

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://abdulaziz:whocares@127.0.0.1:5432/capstone"
+export DATABASE_URL="postgres://yofukdumaeifpi:1866537897d11016c35515399eead03f362970ae80725626a4981590158c339c@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d4s2nss0il2m7u"
 echo "setup.sh script executed successfully!"

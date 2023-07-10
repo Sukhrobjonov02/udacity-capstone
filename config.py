5cc9bd7a4e22b4382737c7c362cbc57fce42a5fd
Abdulaziz <sukhrobjonov02@gmail.com>
2023-07-10 15:12:42 +0500

    fixing database-url

--- config.py
+++ config.py
@@ -8,9 +8,10 @@ DEBUG = True
 
 # Connect to the database
 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')	
-DB_USER = os.getenv('DB_USER', 'abdulaziz')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'whocares')
-DB_NAME = os.getenv('DB_NAME', 'capstone')
+# DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')	
+# DB_USER = os.getenv('DB_USER', 'abdulaziz')
+# DB_PASSWORD = os.getenv('DB_PASSWORD', 'whocares')
+# DB_NAME = os.getenv('DB_NAME', 'capstone')
 
-DATABASE_URL = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+# DATABASE_URL = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+DATABASE_URL = 'postgres://yofukdumaeifpi:1866537897d11016c35515399eead03f362970ae80725626a4981590158c339c@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d4s2nss0il2m7u'

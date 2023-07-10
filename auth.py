df633ceecd43b673200c5b1b1c42ede29015d579
Abdulaziz <sukhrobjonov02@gmail.com>
2023-07-10 19:33:24 +0500

    fixing errors

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 from dotenv import dotenv_values
 
 
-config = dotenv_values("/.env")
+config = dotenv_values(".env")
 
 AUTH0_DOMAIN = config["AUTH0_DOMAIN"]
 ALGORITHMS = config["ALGORITHMS"]
